7d7eb048baca8abf1fad8dcacd9608358397bd8e
iam-donlarry <adekunlequadri3@gmail.com>
2025-08-29 15:13:31 +0100

    created the home page and added latest movies from using tmdb

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image,ScrollView,Text, View } from "react-native";
-import SearchBar  from "@/components/SearchBar";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const { 
+      data: movies, 
+      loading: loadingMovies, 
+      error: errorMovies}  = useFetch(() => fetchMovies({ 
+      query: ''
+  }))
+  
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -14,13 +25,33 @@ export default function Index() {
         <ScrollView className="flex-1" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight:"100%",paddingBottom: 10 }}>
           <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-          <View className="flex-1 mt-5">
-            <SearchBar  onPress={() => router.push("/search")}
-              placeholder="What do you want to watch?"/>
-          </View>
-        </ScrollView>
+          {loadingMovies ? (
+            <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center"/>
+          ) : errorMovies ? (
+            <Text>Error: {errorMovies?.message}</Text>
+          ) :(
+            <View className="flex-1 mt-5">
+              <SearchBar  onPress={() => router.push("/search")}
+                placeholder="What do you want to watch?"/>
 
-        
+                <>  
+                  <Text className="text-white text-lg font-bold mt-5 mb-3">Latest Movies</Text>
+
+                  <FlatList
+                    data={movies}
+                    renderItem={({ item }) => (
+                      <MovieCard {...item} />)}
+                    keyExtractor={(item) => item.id.toString()}
+                    numColumns={3}
+                    columnWrapperStyle={{ justifyContent: 'flex-start', gap: 20, paddingRight: 5, marginBottom: 10 }}
+                    className="mt-2 pb-32"
+                    scrollEnabled={false}   
+                  />
+                </>  
+            </View>
+          ) }
+          
+        </ScrollView>  
     </View>
   );
 }
---
+++ services/api.ts
@@ -0,0 +1,40 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+    
+}
+export const fetchMovies = async ({ query}: { query: string }) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch( endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers
+    });
+
+    if(!response.ok) {
+        //@ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
+    }  
+    const data = await response.json();
+    return data.results;
+}
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//  method: 'GET',
+//  headers: {
+//    accept: 'application/json',
+//    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWFiZTE5ZjVlZDkwOTA4MGVhMzY5YzU3MGI3NWU5MiIsIm5iZiI6MTc1NTUzMjIyMi42NDIsInN1YiI6IjY4YTM0YmJlMGU2ZDlmODNkMjYzMmJlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fsoRzjDqsgbTWYcSI4D_xcPcT2kr03Irfk_t9HdwJck'
+//  }
+//};
+
+//fetch(url, options)
+//  .then(res => res.json())
+//  .then(json => console.log(json))
+//  .catch(err => console.error(err));
